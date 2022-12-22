db21aa9826f77c330893eead4faa98ce074c788d
laraxlara <107651453+laraxlara@users.noreply.github.com>
2022-12-22 19:10:38 +0100

    CV file changed

--- server.js
+++ server.js
@@ -48,19 +48,19 @@ app.get('/send', async (req, res, next) => {
       service: "Gmail",
       auth: {
           type: 'OAuth2',
-          user: process.env.GMAIL_ADRESS,
-          clientId: process.env.CLIENT_ID,
-          clientSecret: process.env.CLIENT_SECRET,
-          refreshToken: process.env.REFRESH_TOKEN,
-          accessToken: process.env.accessToken
+          user: 'laraa.celic7@gmail.com',
+          clientId: '327191201635-qk39c5c9rhadl959d4g6o2pbfdfckisc.apps.googleusercontent.com',
+          clientSecret: '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',
+          refreshToken: '1//04xAGALWDT2_VCgYIARAAGAQSNwF-L9IrNbDzasw0t4dDoSUs9Q2Y_35WOJj7p-4wi3fOy1mxpxltpjpkl87uZneGeTuICQLMeTw',
+          accessToken: 'ya29.a0AX9GBdXEVhViveqzPZcx80yZPseawwz_QGZrE1VIVDsjyOzDmCUN79z7LlmO1qY_GxOApfjRT4Pr6YefvQktltCZOZwcUG22mtUpVjiYIkL3NWarbDHna351HNoOrAiKCpmOqYuCRfOgeWEmoJRNHnnmFtP99MEaCgYKAUsSAQASFQHUCsbCwevZYJSMLZiDgsfNWGu-Cw0166'
       },
       tls: {
         rejectUnauthorized: false
       }
       })
     await transport.sendMail({
-        from: process.env.GMAIL_ADRESS,
-        to: process.env.GMAIL_ADRESS,
+        from: 'laraa.celic7@gmail.com',
+        to: 'laraxlara97@gmail.com',
         subject: "Website",
         html: `<div className="email">
         <h1>Hello</h1>
